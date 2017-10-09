c3478b44276659e39e4f7fbbb5c78b13ca77c34e
= <nicholasdewey@openmailbox.org>
2017-10-08 21:31:01 -0400

    initial testing of saf access code

--- app/src/main/java/com/dewdrop623/androidcrypt/MainActivityFragment.java
+++ app/src/main/java/com/dewdrop623/androidcrypt/MainActivityFragment.java
@@ -1,8 +1,15 @@
 package com.dewdrop623.androidcrypt;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +24,11 @@ import android.widget.ImageButton;
  */
 public class MainActivityFragment extends Fragment {
 
+    private static final int FILE_SEARCH_REQUEST_CODE = 623;
+    private static final int WRITE_FILE_PERMISSION_REQUEST_CODE = 440;
+
+    private EditText inputFileEditText;
+    private ImageButton selectInputFileButton;
     private EditText passwordEditText;
     private EditText confirmPasswordEditText;
     private CheckBox showPasswordCheckbox;
@@ -33,21 +45,24 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        inputFileEditText = (EditText) view.findViewById(R.id.inputFileEditText);
+        selectInputFileButton = (ImageButton) view.findViewById(R.id.selectInputFileButton);
         passwordEditText = (EditText) view.findViewById(R.id.passwordEditText);
         confirmPasswordEditText = (EditText) view.findViewById(R.id.confirmPasswordEditText);
         showPasswordCheckbox = (CheckBox) view.findViewById(R.id.showPasswordCheckbox);
         fileDestinationDirectoryEditText = (EditText) view.findViewById(R.id.fileDestinationDirectoryEditText);
         selectDirectoryButton = (ImageButton) view.findViewById(R.id.selectDirectoryButton);
         fileNameEditText = (EditText) view.findViewById(R.id.fileNameEditText);
-        encryptDecryptButton = (Button) view.findViewById(R.id.encryptButton);
-        encryptDecryptButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                positiveButtonOnClick();
-            }
-        });
+        encryptDecryptButton = (Button) view.findViewById(R.id.encryptDecryptButton);
+        encryptDecryptButton.setOnClickListener(encryptDecryptButtonOnClickListener);
         showPasswordCheckbox.setOnCheckedChangeListener(showPasswordCheckBoxOnCheckedChangeListener);
         selectDirectoryButton.setOnClickListener(selectDirectoryButtonOnClickListener);
+        selectInputFileButton.setOnClickListener(selectInputDirectoryButtonOnClickListener);
+
+        setShowPassword(false);
+
+        checkPermissions();
+
         return view;
     }
 
@@ -58,6 +73,13 @@ public class MainActivityFragment extends Fragment {
         }
     };
 
+    private View.OnClickListener selectInputDirectoryButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            performFileSearch();
+        }
+    };
+
     private View.OnClickListener selectDirectoryButtonOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -65,9 +87,13 @@ public class MainActivityFragment extends Fragment {
         }
     };
 
-    private void positiveButtonOnClick() {
-        //TODO implement
-    }
+
+    private View.OnClickListener encryptDecryptButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     private void setShowPassword(boolean showPassword) {
         int inputType;
@@ -78,5 +104,27 @@ public class MainActivityFragment extends Fragment {
         }
         passwordEditText.setInputType(inputType);
         confirmPasswordEditText.setInputType(inputType);
+    }
+
+    public void performFileSearch() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, FILE_SEARCH_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_SEARCH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Log.wtf("MainActivityFragment.java:asdevas", uri.toString());
+            inputFileEditText.setText(uri.toString());
+        }
+    }
+    //check for the necessary permissions. destroy and recreate the activity if permissions are asked for so that the files (which couldn't be seen previously) will be displayed
+    public void checkPermissions() {
+        if(ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_FILE_PERMISSION_REQUEST_CODE);
+        }
     }
 }
