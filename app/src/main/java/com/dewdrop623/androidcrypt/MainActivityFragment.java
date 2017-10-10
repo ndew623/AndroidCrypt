33b3b224ac2d8f96e5543a0b9f5b390d83105a2d
= <nicholasdewey@openmailbox.org>
2017-10-09 20:02:10 -0400

    more storage access framework work

--- app/src/main/java/com/dewdrop623/androidcrypt/MainActivityFragment.java
+++ app/src/main/java/com/dewdrop623/androidcrypt/MainActivityFragment.java
@@ -9,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +17,10 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -27,7 +30,10 @@ public class MainActivityFragment extends Fragment {
     private static final int FILE_SEARCH_REQUEST_CODE = 623;
     private static final int WRITE_FILE_PERMISSION_REQUEST_CODE = 440;
 
-    private EditText inputFileEditText;
+    private Uri inputFileUri = null;
+    private Uri outputFileUri = null;
+
+    private TextView inputFileTextView;
     private ImageButton selectInputFileButton;
     private EditText passwordEditText;
     private EditText confirmPasswordEditText;
@@ -45,7 +51,7 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        inputFileEditText = (EditText) view.findViewById(R.id.inputFileEditText);
+        inputFileTextView = (TextView) view.findViewById(R.id.inputFileTextView);
         selectInputFileButton = (ImageButton) view.findViewById(R.id.selectInputFileButton);
         passwordEditText = (EditText) view.findViewById(R.id.passwordEditText);
         confirmPasswordEditText = (EditText) view.findViewById(R.id.confirmPasswordEditText);
@@ -91,9 +97,54 @@ public class MainActivityFragment extends Fragment {
     private View.OnClickListener encryptDecryptButtonOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
+            if (validationCheck()) {
+                try {
+                    StorageAccessFrameworkHelper.getUriInputStream(referenceToThisForAnonymousClassButtonListeners(), inputFileUri).close();
+                } catch (IOException ioe) {
+                    showError(R.string.ioexception);
+                    ioe.printStackTrace();
+                }
+            }
         }
     };
+
+    public void performFileSearch() {
+        StorageAccessFrameworkHelper.safOpenFile(this, FILE_SEARCH_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_SEARCH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                inputFileUri = data.getData();
+                inputFileTextView.setText(inputFileUri.getEncodedPath());
+        } else {
+            showError(R.string.storage_access_response_not_result_ok);
+        }
+    }
+    //check for the necessary permissions. destroy and recreate the activity if permissions are asked for so that the files (which couldn't be seen previously) will be displayed
+    public void checkPermissions() {
+        if(ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_FILE_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /*
+    * Display an error to the user.
+    * */
+    private void showError(String error) {
+        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+    * Display an error to the user.
+    * */
+    private void showError(int stringId) {
+        Toast.makeText(getContext(), stringId, Toast.LENGTH_SHORT).show();
+    }
+
+    private MainActivityFragment referenceToThisForAnonymousClassButtonListeners() {
+        return this;
+    }
 
     private void setShowPassword(boolean showPassword) {
         int inputType;
@@ -106,25 +157,15 @@ public class MainActivityFragment extends Fragment {
         confirmPasswordEditText.setInputType(inputType);
     }
 
-    public void performFileSearch() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
-        startActivityForResult(intent, FILE_SEARCH_REQUEST_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == FILE_SEARCH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Uri uri = data.getData();
-            Log.wtf("MainActivityFragment.java:asdevas", uri.toString());
-            inputFileEditText.setText(uri.toString());
+    /*
+    * check if user input is valid before trying to do an operation, return true if it is. Also, notify users of any issues.
+    * */
+    private boolean validationCheck() {
+        boolean valid = true;
+        if (inputFileUri == null) {
+            showError(R.string.no_input_file_selected);
+            valid = false;
         }
-    }
-    //check for the necessary permissions. destroy and recreate the activity if permissions are asked for so that the files (which couldn't be seen previously) will be displayed
-    public void checkPermissions() {
-        if(ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
-            ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_FILE_PERMISSION_REQUEST_CODE);
-        }
+        return valid;
     }
 }
---
+++ app/src/main/java/com/dewdrop623/androidcrypt/StorageAccessFrameworkHelper.java
@@ -0,0 +1,35 @@
+package com.dewdrop623.androidcrypt;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * provides access to the StorageAccessFramework
+ */
+
+public class StorageAccessFrameworkHelper {
+
+    /*
+    * start an Storage Access Framework activity for selecting the input file
+     */
+    public static void safOpenFile(MainActivityFragment mainActivityFragment, final int REQUEST_CODE) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        mainActivityFragment.startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    /*
+    * return an input stream for a given Uri
+    * */
+    public static InputStream getUriInputStream (MainActivityFragment mainActivityFragment, Uri uri) throws IOException {
+        return mainActivityFragment.getActivity().getContentResolver().openInputStream(uri);
+    }
+}
