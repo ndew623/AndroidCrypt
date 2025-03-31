2ad722682d4188650e7667050ce3adc78b785387
nick <nick@androiddev.fedora>
2025-03-31 13:27:54 -0400

    initial build attempt. has cpuid problem.

---
+++ app/src/main/cpp/androidcrypt.cpp
@@ -0,0 +1,18 @@
+#include <terra/aescrypt/engine/encryptor.h>
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("androidcrypt");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("androidcrypt")
+//      }
+//    }
